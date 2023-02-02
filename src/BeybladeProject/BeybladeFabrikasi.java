bdac2171d10c52ff8f180f18646b178065c761d2
Berkay <berkay.tr35@hotmail.com>
2023-02-02 17:28:31 +0300

    Basic Beyblade project with polymorphism

--- src/BeybladeProject/BeybladeFabrikasi.java
+++ src/BeybladeProject/BeybladeFabrikasi.java
@@ -3,16 +3,16 @@ package BeybladeProject;
 public class BeybladeFabrikasi {
 
     public Beyblade beybladeUret(String beybladeTuru){
-        if (beybladeTuru.equals("Dragon")){
+        if (beybladeTuru.equalsIgnoreCase("Dragon")){
             return new Dragon("Takao",800,300,"Mavi Ejderha","Kutsal canavarla konuşma");
         }
-        else if (beybladeTuru.equals("Dranza")) {
+        else if (beybladeTuru.equalsIgnoreCase("Dranza")) {
             return new Dranza("Kai",600,400,"Kırmızı Anka Kuşu");
         }
-        else if (beybladeTuru.equals("Drayga")) {
+        else if (beybladeTuru.equalsIgnoreCase("Drayga")) {
             return new Drayga("Rei",800,250,"Beyaz Kaplan");
         }
-        else if (beybladeTuru.equals("Draciel")) {
+        else if (beybladeTuru.equalsIgnoreCase("Draciel")) {
             return new Draciel("Max",400,1000,"Kara Kaplumbağa");
         }
         else {
